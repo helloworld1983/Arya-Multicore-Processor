e0e5748a711eb98b8204920c488dbb5d355e329f
Manan Hemant Patel <usc533bk@users.isi.deterlab.net>
2014-03-10 18:21:42 -0700

    removing script folder and adding mem_read.pl

---
+++ lib/scripts/mem_read.pl
@@ -0,0 +1,29 @@
+#!usr/bin/perl
+use strict;
+use warnings;
+
+my $COMMAND = 0x2000300;
+my $DATA_HIGH = 0x2000310;
+my $DATA_LOW = 0x2000314;
+my $DATA_ADDR = 0x200030c;
+
+my $file_name = "mem_data.txt";
+open (MYFILE, '>', $file_name)
+  or die "cannot open file to write!";
+
+print MYFILE "No."."\t"."DATA";
+
+system("regwrie $COMMAND 0x204"); 
+
+my $location = 512;
+while ($location != 1024) {
+
+        #print "addr = $i\tdata = 0x$line[0] 0x$line[1]\n";
+        system("regwrite $DATA_ADDR $location");          # address of where to program
+        #system("regread $DATA_HIGH | tee -a output.txt");
+        system("regread $DATA_LOW | tee -a output.txt");
+        #print MYFILE ($location - 512)."\t".$data_high.",".$data_low."\n";
+        #sleep(0.1);
+        select(undef, undef, undef, 0.1);
+        $location++;
+}
